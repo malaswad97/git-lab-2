f2f40d3b9ce7fb3244774e30784279587e0c9d86
Mohammed Alaswad <ma907814@ohio.edu>
2019-05-17 15:53:25 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Mohammed Alaswad
+ *        Date: 05/17/2019
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
